80f01b1f5f21715fae05a942a2247dddc5474bb0
Dražen Gavrić <gavrinjo@gmx.com>
2018-09-07 11:03:53 +0200

    corrected IF statement
    corrected IF statement for fetch "job-company" list

--- scraper.py
+++ scraper.py
@@ -3,7 +3,7 @@ from requests import get
 import re
 
 #keyword = input("Input search keyword ->->->->->")
-base_url = f"https://www.moj-posao.net/Pretraga-Poslova/?keyword=vozac&area=2&category="
+base_url = f"https://www.moj-posao.net/Pretraga-Poslova/?keyword=projektant&area=2&category="
 
 print("\nsearch results\n")
 
@@ -20,19 +20,45 @@ except AttributeError:
 url_list = ["{}&page={}".format(base_url, str(page)) for page in range(1, num_pages + 1)]
 
 stack = []
-company_lst=[]
-position_lst=[]
-location_lst=[]
-deadline_lst=[]
-href_lst=[]
+company_lst = []
+position_lst = []
+location_lst = []
+deadline_lst = []
+href_lst = []
+
+
+# for testing purposis
+
+"""
+for company in html_soup.find_all(True, {"class": ["job-position", "job-title"]}):
+    if company.get("class")[0] == "job-position":
+        try:
+            print(company.parent.parent.parent.a.img.get("title"))
+        except AttributeError:
+            print(company.parent.parent.parent.img.get("title"))
+    else:
+        print(company.find_next_sibling("p").text.strip())
+
+
+src = html_soup.find(class_ = "featured")
+#for i in stack:
+#  print(i)
+print(src.prettify())
+"""
+
+# work in progress
 
 for url in url_list:
     rp = get(url)
     soup = bs(rp.text, "html.parser")
-    for company in soup.find_all(True, {"class": ["job-position"]}):
-        company_lst.append(company.parent.parent.parent.a.img.get("title"))
-    for company in soup.find_all(True, {"class": ["job-company"]}):
-        company_lst.append(company.text.strip())
+    for company in html_soup.find_all(True, {"class": ["job-position", "job-title"]}):
+        if company.get("class")[0] == "job-position":
+            try:
+                company_lst.append(company.parent.parent.parent.a.img.get("title"))
+            except AttributeError:
+                company_lst.append(company.parent.parent.parent.img.get("title"))
+        else:
+            company_lst.append(company.find_next_sibling("p").text.strip())
     for position in soup.find_all(True, {"class": ["job-position", "job-title"]}):
         position_lst.append(position.text.strip())
     for location in soup.find_all(True, {"class": ["job-location"]}):
@@ -49,5 +75,6 @@ for a, b, c, d, e in zip(company_lst, position_lst, location_lst, deadline_lst, 
         stack.append(dict(company=a, position=b, location=c, deadline=d, href=e))
 
 for i in stack:
-  print(i)
-
+  print("\n")
+  for o in i:
+    print(i[o])
