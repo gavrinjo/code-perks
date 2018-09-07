8667fe7e18fcca0de3667ae80e757a48fd7a24a1
Dražen Gavrić <gavrinjo@gmx.com>
2018-09-07 13:17:02 +0200

    fetch function updated
    redone function for fetching data from URL list

--- scraper.py
+++ scraper.py
@@ -19,62 +19,42 @@ except AttributeError:
 # Build up a URL list
 url_list = ["{}&page={}".format(base_url, str(page)) for page in range(1, num_pages + 1)]
 
+# define a dictionary list
 stack = []
+
+# define a lists of dictionary items
 company_lst = []
 position_lst = []
 location_lst = []
 deadline_lst = []
 href_lst = []
 
-
-# for testing purposis
-
-"""
-for company in html_soup.find_all(True, {"class": ["job-position", "job-title"]}):
-    if company.get("class")[0] == "job-position":
-        try:
-            print(company.parent.parent.parent.a.img.get("title"))
-        except AttributeError:
-            print(company.parent.parent.parent.img.get("title"))
-    else:
-        print(company.find_next_sibling("p").text.strip())
-
-
-src = html_soup.find(class_ = "featured")
-#for i in stack:
-#  print(i)
-print(src.prettify())
-"""
-
-# work in progress
-
+# grab items from list of URLs
 for url in url_list:
     rp = get(url)
     soup = bs(rp.text, "html.parser")
-    for company in html_soup.find_all(True, {"class": ["job-position", "job-title"]}):
+    for company in soup.find_all(True, {"class": ["job-position", "job-title"]}):
         if company.get("class")[0] == "job-position":
             try:
                 company_lst.append(company.parent.parent.parent.a.img.get("title"))
             except AttributeError:
                 company_lst.append(company.parent.parent.parent.img.get("title"))
+            deadline_lst.append(company.find_next_sibling(class_ = "deadline").text.strip())
+            href_lst.append(company.parent.get("href"))
         else:
             company_lst.append(company.find_next_sibling("p").text.strip())
-    for position in soup.find_all(True, {"class": ["job-position", "job-title"]}):
-        position_lst.append(position.text.strip())
-    for location in soup.find_all(True, {"class": ["job-location"]}):
-        location_lst.append(location.text.strip())
-    for deadline in soup.find_all(True, {"datetime": [True]}):
-        deadline_lst.append(deadline.text.strip())
-    for href in soup.find_all(True, {"class": ["job-position", "job-title"]}):
-        if href.parent.get("href") is not None:
-          href_lst.append(href.parent.get("href"))
-        else:
-          href_lst.append(href.a.get("href"))
+            deadline_lst.append(company.find_next_sibling(class_ = "deadline").time.text.strip())
+            href_lst.append(company.a.get("href"))
+        position_lst.append(company.text.strip())
+        location_lst.append(company.find_next_sibling(class_ = "job-location").text.strip())
 
+
+# zip dictionary keys wit corresponding values 
 for a, b, c, d, e in zip(company_lst, position_lst, location_lst, deadline_lst, href_lst):
-        stack.append(dict(company=a, position=b, location=c, deadline=d, href=e))
+    stack.append(dict(company=a, position=b, location=c, deadline=d, href=e))
 
+# print out
 for i in stack:
-  print("\n")
-  for o in i:
-    print(i[o])
+	print("\n")
+	for o, h in i.items():
+		print(o + ":", h)
