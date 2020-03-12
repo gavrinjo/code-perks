40a24c1401a413398750bd9033e89fd714bca90f
gavrinjo <gavrinjo@gmx.com>
2020-03-12 13:01:45 +0100

    update class for_test.py - WIP

--- for_test.py
+++ for_test.py
@@ -1,18 +1,18 @@
-from bs4 import BeautifulSoup as bs
+from bs4 import BeautifulSoup as bS
 from requests import get
-# from requests.exceptions import RequestException
+# from requests.exceptions import RequestException, Timeout, TooManyRedirects, HTTPError
 from contextlib import closing
-import json
-import re
-from datetime import datetime, timedelta
+# import json
+# import re
+# from datetime import datetime, timedelta
 
 
 class Source:
-    def __init__(self, source):
-        self.source = source
+    def __init__(self, url):
+        self.url = url
 
-    def url(self):
-        with closing(get(self.source, stream=True)) as f:
+    def raw_data(self):
+        with closing(get(self.url, stream=True)) as f:
             if self.response_check(f):
                 return f.content
             else:
@@ -28,15 +28,16 @@ class Source:
         exit(f"ERROR, check your URLs, invalid response code \"{f.status_code}\"")
 
     def bs_data(self):
-        soup_data = bs(self.url(), "lxml")
-        print(soup_data)
+        soup_data = bS(self.raw_data(), "lxml")
+        return soup_data.prettify()
 
-    def print_source(self):
-        print(self.url())
+
+class Article(Source):
+    def __init__(self, url):
+        Source.__init__(self, url)
 
 
 base_url = "https://www.oglasnik.hr/stanovi-prodaja"
 # ?page=2
-raw_data = Source(base_url)
-# raw_data.print_source()
-raw_data.bs_data()
+raw_data = Article(base_url)
+print(raw_data.bs_data())
